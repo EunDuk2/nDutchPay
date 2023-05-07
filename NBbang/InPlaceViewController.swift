1f2c690a564a87edd4d68abb53330e5c84c2abae
eunsung <dmsqls6789@naver.com>
2023-05-08 00:30:23 +0900

    메뉴 테이블 셀 확장

--- NBbang/InPlaceViewController.swift
+++ NBbang/InPlaceViewController.swift
@@ -30,14 +30,31 @@ extension InPlaceViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let row = place?.menu[indexPath.row].name
+        let row1 = place?.menu[indexPath.row].name
+        var row2: String = "("
+        let row3 = place?.menu[indexPath.row].totalPrice
+        
+        for i in 0..<(place?.menu[indexPath.row].enjoyer.count)! {
+            if(i != (place?.menu[indexPath.row].enjoyer.count)!-1) {
+                row2 += (place?.menu[indexPath.row].enjoyer[i].name)! + ", "
+            } else {
+                row2 += (place?.menu[indexPath.row].enjoyer[i].name)!
+            }
+            
+        }
+        row2 += ")"
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "MenuTableCell") as! MenuTableCell
         
-        cell.lblName.text = row
+        cell.lblName.text = row1
+        cell.lblEnjoyer.text = row2
+        cell.lblTotalPrice.text = String(row3!) + "(원)"
         
         return cell
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100 // 고정된 높이 값을 반환합니다.
+    }
     
 }
--- NBbang/MenuTableCell.swift
+++ NBbang/MenuTableCell.swift
@@ -5,4 +5,7 @@ class MenuTableCell: UITableViewCell {
     
     @IBOutlet var lblName: UILabel!
     
+    @IBOutlet var lblEnjoyer: UILabel!
+    
+    @IBOutlet var lblTotalPrice: UILabel!
 }
