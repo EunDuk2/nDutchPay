19f6b9e47d667b1f2e08dca4b634f82d6da8cb21
eunsung <dmsqls6789@naver.com>
2023-05-07 20:38:13 +0900

    메뉴추가에서 친구추가테이블 틀 구현

--- NBbang/AddMenuViewController.swift
+++ NBbang/AddMenuViewController.swift
@@ -3,4 +3,26 @@ import RealmSwift
 
 class AddMenuViewController: UIViewController {
     
+    
+    
+    @IBAction func onSubmit(_ sender: Any) {
+        
+    }
+    @IBAction func onCancel(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    
+}
+
+extension AddMenuViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AddMenuTableCell") as! AddMenuTableCell
+        
+        return cell
+    }
+    
 }
