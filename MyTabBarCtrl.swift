6fa881bf4f732be73ae9700c5ccf217dde80e0a0
eunsung <dmsqls6789@naver.com>
2023-05-24 21:47:15 +0900

    메인 화면 디자인 구현, 탭바 가운데 버튼

---
+++ MyTabBarCtrl.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+
+class MyTabBarCtrl: UITabBarController, UITabBarControllerDelegate {
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.delegate = self
+   }
+}
--- NBbang/ViewController.swift
+++ NBbang/ViewController.swift
@@ -1,9 +1,3 @@
-//
-//  ViewController.swift
-//  NBbang
-//
-//  Created by EUNSUNG on 2023/03/28.
-//
 import RealmSwift
 import UIKit
 
@@ -11,34 +5,60 @@ class ViewController: UIViewController {
     
     @IBOutlet var table: UITableView!
     
+    let color = UIColor(hex: "#4364C9")
     let realm = try! Realm()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationSetting()
+        
         firstLaunch()
         print(Realm.Configuration.defaultConfiguration.fileURL!)
         
-        //saveDB()
-        //updateDB()
-        //getDB()
-        //deleteDB(i: 0)
-        //eraseDB()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         if let tableView = table {
-                tableView.reloadData()
-            }
+            tableView.reloadData()
+        }
     }
     
+    @objc func navigationSetting() {
+        let navigationBarAppearance = UINavigationBarAppearance()
+        navigationBarAppearance.backgroundColor = color
+        navigationController!.navigationBar.standardAppearance = navigationBarAppearance
+        navigationController!.navigationBar.scrollEdgeAppearance = navigationBarAppearance
+        
+        if let titleView = navigationItem.titleView as? UILabel {
+            titleView.textColor = .white
+            titleView.font = UIFont(name: "SeoulNamsanCM", size: 21)
+        } else {
+            let titleLabel = UILabel()
+            titleLabel.text = "파티 목록"
+            titleLabel.textColor = .white
+            titleLabel.font = UIFont(name: "SeoulNamsanCM", size: 21)
+            navigationItem.titleView = titleLabel
+        }
+        let addButtonImage = UIImage(named: "icon_menu.png")?.withRenderingMode(.alwaysOriginal)
+        let addButton = UIBarButtonItem(title: "", image: addButtonImage, target: self, action: #selector(addButtonTapped))
+
+        navigationItem.rightBarButtonItem = addButton
+    }
+    
+    @objc func addButtonTapped() {
+        guard let na = self.storyboard?.instantiateViewController(withIdentifier: "AddPartyViewController") as? AddPartyViewController else {
+            return
+        }
+        self.present(na, animated: true)
+    }
+
     func firstLaunch() {
         if UserDefaults.standard.bool(forKey: "launchedBefore") == false {
             
             guard let du = self.storyboard?.instantiateViewController(withIdentifier: "AddUserViewController") as? AddUserViewController else {
-                        return
-                    }
+                return
+            }
             du.initBool = true
             
             self.present(du, animated: true)
@@ -51,54 +71,37 @@ class ViewController: UIViewController {
         return realm.objects(Party.self)
     }
     
-    
-    
     func eraseDB() {
         // 아예 Realm 파일 삭제
-                let realmURL = Realm.Configuration.defaultConfiguration.fileURL!
-                let realmURLs = [
-                  realmURL,
-                  realmURL.appendingPathExtension("lock"),
-                  realmURL.appendingPathExtension("note"),
-                  realmURL.appendingPathExtension("management")
-                ]
-                
-                for URL in realmURLs {
-                  do {
-                    try FileManager.default.removeItem(at: URL)
-                  } catch {
-                    // handle error
-                  }
-                }
-
+        let realmURL = Realm.Configuration.defaultConfiguration.fileURL!
+        let realmURLs = [
+            realmURL,
+            realmURL.appendingPathExtension("lock"),
+            realmURL.appendingPathExtension("note"),
+            realmURL.appendingPathExtension("management")
+        ]
+        
+        for URL in realmURLs {
+            do {
+                try FileManager.default.removeItem(at: URL)
+            } catch {
+                // handle error
+            }
+        }
     }
     
-    func addPartyNsaveDB(name:String?) {
-        
+    func addPartyNsaveDB(name: String?) {
         try! realm.write {
             realm.add(Party(name: name))
         }
     }
-
-//    func getDB() {
-//
-//        lbltest.text = party()[1].user[0].name
-//    }
-
-//    func updateDB() {
-//        let getParty = realm.objects(Party.self)
-//        try! realm.write {
-//            getParty[0].name = "eunduk"
-//        }
-//    }
-
-    func deleteDB(i:Int) {
-        //let getParty = realm.objects(Party.self)
+    
+    func deleteDB(i: Int) {
         try! realm.write {
             realm.delete(party()[i])
         }
     }
-
+    
     func deleteAll() {
         try! realm.write {
             realm.deleteAll()
@@ -111,22 +114,19 @@ class ViewController: UIViewController {
 
     @IBAction func addParty(_ sender: Any) {
         guard let na = self.storyboard?.instantiateViewController(withIdentifier: "AddPartyViewController") as? AddPartyViewController else {
-                    return
-                }
+            return
+        }
         self.navigationController?.pushViewController(na, animated: true)
     }
     
     @IBAction func onDeleteDB(_ sender: Any) {
-//        try! realm.write {
-//            realm.delete(party()[party().count-1])
-//        }
         eraseDB()
         table.reloadData()
     }
     
 }
 
-extension ViewController:  UITableViewDelegate, UITableViewDataSource{
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return cnt()
@@ -147,12 +147,11 @@ extension ViewController:  UITableViewDelegate, UITableViewDataSource{
         var userList: String = ""
         userList += String(party()[index].user.count) + "명 ("
         for i in 0..<party()[index].user.count {
-            if(i < party()[index].user.count-1) {
+            if i < party()[index].user.count-1 {
                 userList += party()[index].user[i].name! + ","
             } else {
                 userList += party()[index].user[i].name!
             }
-            
         }
         userList += ")"
         return userList
@@ -160,19 +159,42 @@ extension ViewController:  UITableViewDelegate, UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         guard let na = self.storyboard?.instantiateViewController(withIdentifier: "InPartyViewController") as? InPartyViewController else {
-                    return
-                }
+            return
+        }
         na.index = indexPath.row
-
-        //du.modalPresentationStyle = .fullScreen
-        //self.present(du, animated: true)
         self.navigationController?.pushViewController(na, animated: true)
-        
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 70 // 고정된 높이 값을 반환합니다.
     }
+}
 
+extension UIColor {
+    convenience init?(hex: String) {
+        let r, g, b, a: CGFloat
+        
+        if hex.hasPrefix("#") {
+            let start = hex.index(hex.startIndex, offsetBy: 1)
+            let hexColor = String(hex[start...])
+            
+            if hexColor.count == 6 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+                
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xFF0000) >> 16) / 255.0
+                    g = CGFloat((hexNumber & 0x00FF00) >> 8) / 255.0
+                    b = CGFloat(hexNumber & 0x0000FF) / 255.0
+                    a = 1.0
+                    
+                    self.init(red: r, green: g, blue: b, alpha: a)
+                    return
+                }
+            }
+        }
+        
+        return nil
+    }
 }
 
