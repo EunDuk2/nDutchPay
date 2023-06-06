2f8cd248146d9820e5b0980b3caeb815746c1872
eunsung <dmsqls6789@naver.com>
2023-06-07 03:17:17 +0900

    앱아이콘 및 시작화면

--- NBbang/AppDelegate.swift
+++ NBbang/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        sleep(1)
         // Override point for customization after application launch.
         UINavigationBar.appearance().tintColor = UIColor.white
         KakaoSDK.initSDK(appKey: "d8a4f3fa850222b7dafa22f08a016500")
